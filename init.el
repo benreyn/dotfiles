5cb5a895eb30939319110e31c0677f93249aacf3
BenjaminReynolds <breyno127@gmail.com>
2015-07-17 20:40:02 -0400

    added web-mode

--- init.el
+++ init.el
@@ -19,11 +19,6 @@
 (when is-mac
   (setq mac-command-modifier 'control))
 
-;; Setup environment variables from the user's shell.
-(when is-mac
-  (require-package 'exec-path-from-shell)
-  (exec-path-from-shell-initialize))
-
 ;; Load Paths
 (add-to-list 'load-path "~/.emacs.d/themes/")
 
@@ -31,25 +26,31 @@
 (require 'color-theme-tomorrow)
 (color-theme-tomorrow--define-theme night)
 
-;; Install packages with error handling for my work computer
-(setq package-list '(popup
-		     auto-complete
-		     chess
-		     emmet-mode
-		     php-mode
-		     yasnippet))
+;; Install packages
+(setq package-list '(flymake
+		flymake-css
+		flymake-php 
+		flymake-python-pyflakes 
+		flymake-ruby 
+		flymake-easy
+		magit
+		multiple-cursors
+		php-mode
+		yasnippet
+		auto-complete
+		popup
+		web-mode
+		emmet-mode))
 (setq package-archives '(("elpa" . "http://tromey.com/elpa/")
                          ("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.org/packages/")))                  
 (package-initialize)
 (condition-case err
-(unless package-archive-contents
   (package-refresh-contents))
 (dolist (package package-list)
   (unless (package-installed-p package)
     (package-install package)))
-(debug error) nil)
 
 ;; Version control and backup file management
 (setq
@@ -69,10 +70,40 @@
                   week))
       (message "%s" file)
       (delete-file file))))
+      
+;; Multiple cursors setup
+(require 'multiple-cursors)
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+
+;; Setup web-mode
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(setq web-mode-engines-alist
+      '(("php"    . "\\.phtml\\'")
+        ("blade"  . "\\.blade\\."))
+      )
+(setq web-mode-enable-current-element-highlight t)
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+  )
+
 
 ;; Add hooks
 (add-hook 'prog-mode-hook 'electric-pair-mode)
 (add-hook 'prog-mode-hook 'auto-complete-mode)
 (add-hook 'prog-mode-hook 'linum-mode)
-(add-hook 'sgml-mode-hook 'emmet-mode)
-(add-hook 'css-mode-hook  'emmet-mode)
+(add-hook 'web-mode-hook 'emmet-mode)
+(add-hook 'web-mode-hook 'my-web-mode-hook)

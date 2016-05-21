db4d2efe85906a8b329fb75129cd5e1c9fc09ed6
Benjamin Reynolds <breyno127@gmail.com>
2016-05-21 15:31:18 -0400

    Keeping everything in old-setup for now.

--- custom/old-setup.el
+++ custom/old-setup.el
@@ -37,3 +37,13 @@
                   week))
       (message "%s" file)
       (delete-file file))))
+
+;; Multiple cursors setup
+(require 'multiple-cursors)
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+
+;; Magit
+(global-set-key (kbd "C-x g") 'magit-status)

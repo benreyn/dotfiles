471a96996f31a43bd459379270faed1f40b8b505
Benjamin Reynolds <breyno127@gmail.com>
2016-05-29 08:38:59 -0400

    Some clean up and install helm

--- custom/old-setup.el
+++ custom/old-setup.el
@@ -20,61 +20,7 @@
   (setq mac-command-modifier 'meta))
 
 ;; Version control and backup file management
-(setq auto-save-default nil)
-(defconst emacs-tmp-dir (format "%s/%s%s/" temporary-file-directory "emacs" (user-uid)))
-(setq backup-directory-alist
-      `((".*" . ,emacs-tmp-dir)))
-(setq auto-save-file-name-transforms
-      `((".*" ,emacs-tmp-dir t)))
-(setq auto-save-list-file-prefix
-      emacs-tmp-dir)
-(message "Deleting old backup files...")
-(let ((week (* 60 60 24 7))
-      (current (float-time (current-time))))
-  (dolist (file (directory-files temporary-file-directory t))
-    (when (and (backup-file-name-p file)
-               (> (- current (float-time (fifth (file-attributes file))))
-                  week))
-      (message "%s" file)
-      (delete-file file))))
-
-;; Multiple cursors setup
-(require 'multiple-cursors)
-(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
-(global-set-key (kbd "C->") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
-
-;; Magit
-(global-set-key (kbd "C-x g") 'magit-status)
-(global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
-
-;; Add hooks
-(add-hook 'prog-mode-hook 'electric-pair-mode)
-(add-hook 'prog-mode-hook 'auto-complete-mode)
-
-;; Turn off splash screen and mouse interface
-(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
-(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
-(setq inhibit-startup-message t)
-
-;; Show row / column numbers
-(line-number-mode t)
-(column-number-mode t)
-
-;; Are we on a mac?
-(setq is-mac (equal system-type 'darwin))
-
-;; ido mode
-  (ido-mode t)
-  (setq ido-enable-flex-matching t)
-
-;; Set mac command key
-(when is-mac
-  (setq mac-command-modifier 'meta))
-
-;; Version control and backup file management   
+(setq create-lockfiles nil)
 (defconst emacs-tmp-dir (format "%s/%s%s/" temporary-file-directory "emacs" (user-uid)))
 (setq backup-directory-alist
       `((".*" . ,emacs-tmp-dir)))
@@ -128,7 +74,9 @@
 
 (when window-system (set-exec-path-from-shell-PATH))
 
-(add-hook 'prog-mode-hook 'linum-mode)
+;; ember-mode
+(require 'cl)
+(require 'ember-mode)
 (add-hook 'js-mode-hook (lambda () (ember-mode t)))
 (add-hook 'web-mode-hook (lambda () (ember-mode t)))
 
@@ -137,18 +85,6 @@
 
 ;; RSpec mode
 (add-hook 'after-init-hook 'inf-ruby-switch-setup)
-
-;; Gimme my shell
-(defun set-exec-path-from-shell-PATH ()
-  (let ((path-from-shell (replace-regexp-in-string
-                          "[ \t\n]*$"
-                          ""
-                          (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
-    (setenv "PATH" path-from-shell)
-    (setq eshell-path-env path-from-shell) ; for eshell users
-    (setq exec-path (split-string path-from-shell path-separator))))
-
-(when window-system (set-exec-path-from-shell-PATH))
 
 ;; Navigate between windows using Alt-1, Alt-2, Shift-left, shift-up, shift-right
 (windmove-default-keybindings)
@@ -181,3 +117,9 @@
 (setq-default indent-tabs-mode nil)
 (setq-default c-basic-offset 2)
 (setq-default js-indent-level 2)
+
+;; Powerline
+(powerline-default-theme)
+
+;; Helm
+(require 'helm-config)

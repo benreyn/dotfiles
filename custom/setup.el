61bc38d5c4f4e1ee693a1b36237b7cb366977050
Benjamin Reynolds <breyno127@gmail.com>
2016-05-30 09:24:55 -0400

    Add some sensible defaults I will clean up later

--- custom/setup.el
+++ custom/setup.el
@@ -12,8 +12,8 @@
 (setq is-mac (equal system-type 'darwin))
 
 ;; ido mode
-  (ido-mode t)
-  (setq ido-enable-flex-matching t)
+(ido-mode t)
+(setq ido-enable-flex-matching t)
 
 ;; Set mac command key
 (when is-mac
@@ -125,3 +125,76 @@
 (global-set-key (kbd "C-x f") 'fiplr-find-file)
 (setq fiplr-ignored-globs '((directories (".git" ".svn" "node_modules" "tmp" "bower_components" "dist"))
                             (files ("*.jpg" "*.png" "*.zip" "*~"))))
+
+;; Resizing windows should be easy
+(global-set-key (kbd "S-C-<left>") 'shrink-window-horizontally)
+(global-set-key (kbd "S-C-<right>") 'enlarge-window-horizontally)
+(global-set-key (kbd "S-C-<down>") 'shrink-window)
+(global-set-key (kbd "S-C-<up>") 'enlarge-window)
+
+;; Scroll Margins
+(setq scroll-margin 5
+      scroll-preserve-screen-position 1)
+
+;; File path in window name
+(setq-default frame-title-format '((:eval (if (buffer-file-name)
+                                              (abbreviate-file-name (buffer-file-name)) "%f"))))
+
+(scroll-bar-mode 0) ;; Disables the scroll bar
+(setq-default fill-column 80) ;; Sets a 80 character line width
+(setq inhibit-startup-screen t) ;; Don’t display the default splash screen
+(setq large-file-warning-threshold nil) ;; Don’t warn me about opening large files
+(setq x-select-enable-clipboard t) ;; Enable copy/past-ing from clipboard
+(setq system-uses-terminfo nil) ;; Fix weird color escape sequences
+(prefer-coding-system 'utf-8) ;; Prefer UTF-8 encoding
+(fset 'yes-or-no-p 'y-or-n-p) ;; Answer with y and n instead of yes and no
+(setq confirm-kill-emacs 'yes-or-no-p) ;; Ask for confirmation before closing emacs
+(global-auto-revert-mode 1) ;; Always reload the file if it changed on disk
+(setq-default line-spacing 1) ;; A nice line height
+(show-paren-mode 1) ;; Highlight matching parens
+(setq ns-use-srgb-colorspace t) ;; SRGB support for OSX
+(tool-bar-mode 0) ;; Disable the toolbar
+
+;; Open splits horizontally
+(setq split-height-threshold 0)
+(setq split-width-threshold nil)
+
+(set-fringe-mode '(10 . 0)) ;; Show a nice fringe
+
+;; Remove the fringe indicators
+(when (boundp 'fringe-indicator-alist)
+  (setq-default fringe-indicator-alist
+        '(
+          (continuation . nil)
+          (overlay-arrow . nil)
+          (up . nil)
+          (down . nil)
+          (top . nil)
+          (bottom . nil)
+          (top-bottom . nil)
+          (empty-line . nil)
+          (unknown . nil))))
+
+;; Default frame size
+(setq initial-frame-alist
+      '((top . 10) (left . 50) (width . 185) (height . 55)))
+
+;; Store all backup and autosave files in the tmp dir
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
+;; Always use two spaces to indentation
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 2)
+(setq-default c-basic-offset 2)
+(setq css-indent-offset 2)
+(setq js-indent-level 2)
+(setq web-mode-markup-indent-offset 2)
+(setq web-mode-code-indent-offset 2)
+(setq web-mode-css-indent-offset 2)
+
+;; Making dabbrev a bit nicer
+(setq dabbrev-abbrev-skip-leading-regexp ":")
+(setq dabbrev-backward-only t)

07e14ad645416ef751dfa05880366b74acf98f6b
Benjamin Reynolds <breyno127@gmail.com>
2016-08-15 20:27:55 -0400

    Add global gitignore

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ ln -s ~/.dotfiles/bashrc ~/.bashrc
 ln -s ~/.dotfiles/gitignore ~/.gitignore
 ln -s ~/.dotfiles/git-completion.sh ~/.git-completion.sh
 ln -s ~/.dotfiles/git-prompt.sh ~/.git-prompt.sh
+ln -s ~/.dotfiles/gitignore ~/.gitignore
 ln -s ~/.dotfiles/emacs.d ~/.emacs.d
 
 # Link .bash_profile -> .bashrc

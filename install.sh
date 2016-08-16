21c0348773bd2c64e35911a951362425ca636a32
Benjamin Reynolds <breyno127@gmail.com>
2016-08-15 20:24:26 -0400

    Add install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Link files from dotfiles
+ln -s ~/.dotfiles/bashrc ~/.bashrc
+ln -s ~/.dotfiles/gitignore ~/.gitignore
+ln -s ~/.dotfiles/git-completion.sh ~/.git-completion.sh
+ln -s ~/.dotfiles/git-prompt.sh ~/.git-prompt.sh
+ln -s ~/.dotfiles/emacs.d ~/.emacs.d
+
+# Link .bash_profile -> .bashrc
+rm -f ~/.bash_profile
+ln -s ~/.bashrc ~/.bash_profile

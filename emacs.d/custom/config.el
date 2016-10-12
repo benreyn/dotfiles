81b06018e95da89f8ce4e5abb2379926f6caaf85
Benjamin Reynolds <breyno127@gmail.com>
2016-10-12 10:51:28 -0500

    Find and replace in project with ag

--- emacs.d/custom/config.el
+++ emacs.d/custom/config.el
@@ -274,6 +274,16 @@
             (goto-char (point-min))
             (replace-regexp key value)))))
 
+(eval-after-load 'ag
+  '(define-key ag-mode-map
+    (kbd "C-x C-q") 'wgrep-change-to-wgrep-mode))
+
+(eval-after-load 'wgrep
+  '(define-key grep-mode-map
+    (kbd "C-c C-c") 'wgrep-finish-edit))
+
+(setq wgrep-auto-save-buffer t)
+
 (global-set-key (kbd "C-x 2") 'hrs/split-window-below-and-switch)
 (global-set-key (kbd "C-x 3") 'hrs/split-window-right-and-switch)
 

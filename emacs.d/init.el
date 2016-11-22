4e76bd8c30c27aaa432f2157c56f9376ff8eae82
Benjamin Reynolds <breyno127@gmail.com>
2016-11-22 11:49:00 -0600

    Im just going to leave this here...

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,3 +1,6 @@
+;; Leave this here to make package.el happy
+(package-initialize)
+
 ;; Cask
 (require 'cask "/usr/local/share/emacs/site-lisp/cask/cask.el")
 (cask-initialize)
@@ -10,6 +13,9 @@
 ;; Color theme
 (require 'color-theme-tomorrow)
 (color-theme-tomorrow--define-theme night)
+
+;; Prevent writing package-selected-packages
+(defun package--save-selected-packages (&rest opt) nil)
 
 ;; Load me some files!
 (load "config.el")

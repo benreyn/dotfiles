a6d20c686af4aad818299fcecc06dff12dd17f82
Benjamin Reynolds <breyno127@gmail.com>
2017-03-10 20:41:38 -0600

    Update install script

--- install.sh
+++ install.sh
@@ -5,13 +5,13 @@ ln -s ~/.dotfiles/bashrc ~/.bashrc
 ln -s ~/.dotfiles/inputrc ~/.inputrc
 ln -s ~/.dotfiles/gitignore ~/.gitignore
 ln -s ~/.dotfiles/gitconfig ~/.gitconfig
-ln -s ~/.dotfiles/emacs.d ~/.emacs.d
 ln -s ~/.dotfiles/offlineimap ~/.offlineimap
 ln -s ~/.dotfiles/offlineimaprc ~/.offlineimaprc
 ln -s ~/.dotfiles/msmtprc ~/.msmtprc
 ln -s ~/.dotfiles/karabiner/karabiner.json ~/.config/karabiner/karabiner.json
 ln -s ~/.dotfiles/amethyst ~/.amethyst
 ln -s ~/.dotfiles/hammerspoon ~/.hammerspoon
+ln -s ~/.dotfiles/spacemacs ~/.spacemacs
 
 # Link .bash_profile -> .bashrc
 rm -f ~/.bash_profile

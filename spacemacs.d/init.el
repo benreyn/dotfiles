61e38ac89ee91fee9ed2a71f9b6355eb66640bda
Benjamin Reynolds <breyno127@gmail.com>
2017-07-10 13:20:46 -0500

    [spacemacs] Migrate mu4e configuration to contexts
    As of sly20bnr/spacemacs@27e4c43c67597ec992e665e827eaa39bc0df90b6 spacemacs no
    longer supports the old multiple accounts hack, so I am migrating my mu4e
    configuration to use mu4e-contexts as recommended in syl20bnr/spacemacs#4836

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -38,7 +38,6 @@ values."
      yaml
 
      (mu4e :variables
-           mu4e-account-alist t
            mu4e-enable-notifications t
            mu4e-enable-mode-line t)
      (ruby :variables
@@ -167,6 +166,58 @@ you should place your code here."
   (global-emojify-mode)
 
   ;; mu4e
+  (with-eval-after-load 'mu4e
+    (setq mu4e-get-mail-command "offlineimap"
+          mu4e-update-interval 180
+          mu4e-compose-signature-auto-include nil
+          mu4e-sent-messages-behavior 'sent
+          mu4e-view-show-images t
+          mu4e-view-show-addresses t
+          mu4e-maildir "~/mail"
+          message-send-mail-function 'message-send-mail-with-sendmail
+          sendmail-program "/usr/local/bin/msmtp")
+
+    (setq mu4e-maildir-shortcuts
+          '(("/personal/INBOX" . ?p)
+            ("/work/INBOX"     . ?w)))
+
+    (setq mu4e-contexts
+          `( ,(make-mu4e-context
+          :name "personal"
+          :enter-func (lambda () (mu4e-message "Switch to the personal context"))
+          :match-func (lambda (msg)
+            (when msg
+              (mu4e-message-contact-field-matches msg
+                :to "breyno127@gmail.com")))
+          :vars '(( user-mail-address  . "breyno127@gmail.com" )
+                  ( user-full-name     . "Benjamin Reynolds"   )
+                  ( mu4e-drafts-folder . "/personal/drafts"    )
+                  ( mu4e-sent-folder   . "/personal/sent"      )
+                  ( mu4e-trash-folder  . "/personal/trash"     )
+                  ( mu4e-refile-folder . "/personal/archive"   )
+                  ( mu4e-compose-signature .
+                    (concat
+                      "Thanks\n"
+                      "BR"))))
+             ,(make-mu4e-context
+          :name "work"
+          :enter-func (lambda () (mu4e-message "Switch to the work context"))
+          :match-func (lambda (msg)
+            (when msg
+              (mu4e-message-contact-field-matches msg
+                :to "ben@zaarly.com")))
+          :vars '(( user-mail-address  . "ben@zaarly.com"    )
+                  ( user-full-name     . "Benjamin Reynolds" )
+                  ( mu4e-drafts-folder . "/work/drafts"      )
+                  ( mu4e-sent-folder   . "/work/sent"        )
+                  ( mu4e-trash-folder  . "/work/trash"       )
+                  ( mu4e-refile-folder . "/work/archive"     )
+                  ( mu4e-compose-signature .
+                    (concat
+                      "Benjamin Reynolds\n"
+                      "Software Developer\n"
+                      "Zaarly Inc")))))))
+
   (with-eval-after-load 'mu4e-alert
     (mu4e-alert-set-default-style 'notifier)
     (setq mu4e-alert-interesting-mail-query
@@ -184,37 +235,6 @@ you should place your code here."
   ;; write custom-set-variables to a location I dont care about
   (setq custom-file (concat configuration-layer-private-directory "custom.el"))
 
-  (setq mu4e-account-alist
-        '(("personal"
-           (user-mail-address "breyno127@gmail.com")
-           (user-full-name "Benjamin Reynolds")
-           (mu4e-sent-folder "/personal/sent")
-           (mu4e-trash-folder "/personal/trash")
-           (mu4e-drafts-folder "/personal/drafts")
-           (mu4e-refile-folder "/personal/archive"))
-          ("work"
-           (user-mail-address "ben@zaarly.com")
-           (user-full-name "Benjamin Reynolds")
-           (mu4e-sent-folder "/work/sent")
-           (mu4e-trash-folder "/work/trash")
-           (mu4e-drafts-folder "/work/drafts")
-           (mu4e-refile-folder "/work/archive"))))
-  (mu4e/mail-account-reset)
-
-  (setq mu4e-maildir "~/mail"
-        mu4e-get-mail-command "offlineimap"
-        mu4e-update-interval 180
-        mu4e-compose-signature-auto-include nil
-        mu4e-sent-messages-behavior 'sent
-        mu4e-view-show-images t
-        mu4e-view-show-addresses t)
-
-  (setq mu4e-maildir-shortcuts
-        '(("/personal/INBOX" . ?p)
-          ("/work/INBOX"     . ?w)))
-
-  (setq message-send-mail-function 'message-send-mail-with-sendmail)
-  (setq sendmail-program "/usr/local/bin/msmtp")
 
   (spacemacs|define-custom-layout "@Dev"
     :binding "d")

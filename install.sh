f45a4ba592ff2259b5754a3e875eaf64c983f6be
Benjamin Reynolds <breyno127@gmail.com>
2020-01-12 09:29:31 -0600

    Move Tmux and Git configs to stow

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
+# Install dotfiles
+stow bash
+stow alacritty
+stow tmux
+stow git
+
 # Link files from dotfiles
-ln -s ~/.dotfiles/bashrc ~/.bashrc
 ln -s ~/.dotfiles/inputrc ~/.inputrc
 ln -s ~/.dotfiles/gitignore ~/.gitignore
 ln -s ~/.dotfiles/gitconfig ~/.gitconfig
@@ -9,17 +14,9 @@ ln -s ~/.dotfiles/offlineimap ~/.offlineimap
 ln -s ~/.dotfiles/offlineimaprc ~/.offlineimaprc
 ln -s ~/.dotfiles/msmtprc ~/.msmtprc
 ln -s ~/.dotfiles/karabiner/karabiner.json ~/.config/karabiner/karabiner.json
-ln -s ~/.dotfiles/kwm ~/.kwm
-ln -s ~/.dotfiles/khdrc ~/.khdrc
 ln -s ~/.dotfiles/hammerspoon ~/.hammerspoon
-ln -s ~/.dotfiles/spacemacs.d ~/.spacemacs.d
-ln -s ~/.dotfiles/doom.d ~/.doom.d
-ln -s ~/.dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
 ln -s ~/.dotfiles/nerdbar.widget ~/Library/Application Support/Ubersicht/widgets/nerdbar.widget
 ln -s ~/.dotfiles/gnupg ~/.gnupg
-ln -s ~/.dotfiles/vimrc ~/.vimrc
-ln -s ~/.dotfiles/rbenv/default-gems ~/.rbenv/default-gems
-ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
 
 # Link .bash_profile -> .bashrc
 rm -f ~/.bash_profile
